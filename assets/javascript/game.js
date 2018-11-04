c9d22ea4dd3e8fc8264ea9d436ac05acc7a80997
Carlos <carlos@montima.mx>
2018-11-03 20:00:28 -0600

    final version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -28,9 +28,6 @@ function initializeCrystals() {
         var crystal = $("#crystal-" + (i + 1));
         crystal.attr("crystal-score", crystalScore);
     };
-
-    crystal.html(crystalScore);
-
 }
 
 function counter() {
@@ -56,22 +53,10 @@ $(document).ready(function () {
         initializeTexts();
         initializeCrystals();
     });
-    
+
     $(".crystal").on("click", function () {
         userScore += parseInt($(this).attr("crystal-score"));
         $("#userScore").text(userScore);
         counter();
     });
 });
-
-
-
-  // Speudo code
-// un juego que contenga 4 cristales
-// la computadora tiene que generar un numero random (del 19 al 120)
-// cada cristal tiene que tener un numero random del 1 al 12 solamente
-// cada vez que se gane o pierda se tiene que generar de nuevo un numero random para cada cristal
-// cuando hacemos click a cada cristal, se tienen que ir sumando
-// la cantidad del usuario tiene que igual el resultado RANDOM que genere la computadora (del 19 al 120)
-// si te pasas del total (computerRandom) es cuando pierdes
-// cada juego que se gane o pierda, tiene que sumar un juego en el contador (wins or loses)
